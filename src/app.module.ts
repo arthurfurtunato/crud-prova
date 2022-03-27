62b3584bd26beaee5d2e66d26ed7869d14ebddc1
arthur.furtunato <arthur.furtunato@lais.huol.ufrn.br>
2022-03-27 01:49:57 -0300

    arquivo .env feito, colocando o database pra funcionar, delete ormconfig.json e add database.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ExamModule } from './modules/exam.module';
 import { TypeOrmModule} from '@nestjs/typeorm'
+import { ConfigModule } from '@nestjs/config';
+import { ormConfig } from './database/config/database';
 
 @Module({
-  imports: [ExamModule, TypeOrmModule.forRoot()],
+  imports: [
+    ExamModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(ormConfig())
+  ],
 })
 export class AppModule {}
---
+++ src/database/config/database.ts
@@ -0,0 +1,10 @@
+export const ormConfig = (): any => ({
+    type: process.env.DB_TYPE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    synchronize: true,
+    entities: ["dist/**/*.model.js"],
+  });
